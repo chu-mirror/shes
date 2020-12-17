e78b4d910932c1a0b7a4861ca79c73614ac57e03
chu <tomzz233333@gmail.com>
2020-12-17 21:31:15 +0800

    bug: the misunderstanding at tar -cf

--- gotemp.sh
+++ gotemp.sh
@@ -5,7 +5,8 @@ dest=/mnt/tmp/temp
 [ -d $test ] || exit 1
 
 for f in $@; do
-	[ -d $f ] && tar -cf $f.tar.zip $f && f=$f.tar.zip
-	cp $f $dest
+	[ -d $f ] && tar -cf $f.tar $f &&\
+		zip $f.tar.zip $f.tar && f=$f.tar.zip
+	sudo cp $f $dest
 done
 
