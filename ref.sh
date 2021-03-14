29c4f23c0652d3946070ab9e52a59ceb4e962290
chu <tomzz233333@gmail.com>
2021-03-14 19:42:23 +0800

    add: openning of directory

--- ref.sh
+++ ref.sh
@@ -1,8 +1,21 @@
-#/bin/sh
+#!/bin/sh
 
-refs_path=~/ref
+# if no directory is specified,
+# use $HOME/ref as the default
+ref_path=${1:-$HOME/ref}
+[ -d "$ref_path" ] || mkdir -p "$ref_path"
+cd "$ref_path"
 
-cd $refs_path
-selected=$refs_path/$(echo * | tr ' ' '\n' | dmenu -l 5) 
-[ $selected != $refs_path/ ] && mupdf-gl $selected
+# select an entry from dmenu
+selected=$ref_path/$(echo * | tr ' ' '\n' | dmenu -l 5) 
+
+# if the entry is a directory,
+# invoke ref again on that directory
+[ "$selected" != $ref_path/ ] && { \
+	if [ -d "$selected" ]; then 
+		ref "$selected" 
+	else 
+		mupdf-gl "$selected"
+	fi
+}
 
